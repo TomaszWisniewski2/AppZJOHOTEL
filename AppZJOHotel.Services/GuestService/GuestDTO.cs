ebed9f8e60f1decf3a921d32c3f3f0353302030b
Artur Janiak <Artur.t.janiak@gmail.com>
2023-03-09 19:54:05 +0100

    Przykładowa funkcja

---
+++ AppZJOHotel.Services/GuestService/GuestDTO.cs
@@ -0,0 +1,18 @@
+﻿using AppZJOHotel.Common.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppZJOHotel.Services.GuestService
+{
+    public class GuestDTO
+    {
+        public int? Id { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+}
---
+++ AppZJOHotel.Services/GuestService/GuestService.cs
@@ -0,0 +1,38 @@
+﻿using AppZJOHotel.DAL;
+using AppZJOHotel.DAL.Entities;
+using AppZJOHotel.WEBAPI.Db_Access;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AppZJOHotel.Services.GuestService;
+
+public class GuestService : IGuestService
+{
+    private readonly Func<DatabaseContext> context;
+
+    public GuestService(Func<DatabaseContext> context)
+    {
+        this.context = context;
+    }
+
+
+    public async Task<List<GuestDTO?>> ListGuests()
+    {
+        using var ctx = context();
+        var q = ctx.Guest.AsQueryable().AsNoTracking().Where(x=>x.Id>2);
+        var result = await q.Select(GuestListDTOExpression).ToListAsync();
+   
+        return result;
+    }
+
+    // to niżej ma być na dole
+    #region Expressions
+    internal static readonly Expression<Func<Guest, GuestDTO?>> GuestListDTOExpression = x => new GuestDTO()
+    {
+        Id = x.Id,
+        Email = x.Email,
+        Password = x.Password,
+        Surname = x.Surname
+    };
+    #endregion
+}
